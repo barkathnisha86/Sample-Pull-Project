93d2c71e834c387e0267ae4462c41667f613b4e6
barkathnisha86 <barkathnisha86@gmail.com>
2022-12-06 19:28:04 +0530

    Push project

---
+++ src/org/gitsample/Sample.java
@@ -0,0 +1,12 @@
+package org.gitsample;
+
+public class Sample {
+public static void main(String[] args) {
+	
+	System.out.println("Hameesha");
+	
+	System.out.println("Farhan");
+	
+	System.out.println("Basha");
+}
+}
